93bddcb0e857b64a36598f1b735f6724a96165c0
Braeden Christensen <braeden.j.christensen@gmail.com>
2022-04-07 23:27:24 -0600

    vscode git test

--- ConsolidatedLabsProto/Assets/Scripts/PlayerMovement.cs
+++ ConsolidatedLabsProto/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,20 @@ public class PlayerMovement : MonoBehaviour
 {
     private const float LOWESTMOVEMENTRATE = 10f;
     private const float UPPERSTRAFERATE = 100f;
+    private const float MINROTATIONLIMIT = 10f;
+    private const float MINROTATIONRATE = 10f;
     [Header("Movement Rate Modifiers")]
     [Range(LOWESTMOVEMENTRATE, UPPERSTRAFERATE)]
     [Min(LOWESTMOVEMENTRATE)]
     public float strafeRate = LOWESTMOVEMENTRATE;
     [Min(LOWESTMOVEMENTRATE)]
     public float speed = 10f;
+    [Min(MINROTATIONRATE)]
+    public float rotationRate = MINROTATIONRATE;
 
-    
+    [Min(MINROTATIONLIMIT)]
+    public float rotationLimit = MINROTATIONLIMIT;
+
     [SerializeField]
     private Canvas gameOverCanvas;
     [SerializeField]
@@ -23,9 +29,15 @@ public class PlayerMovement : MonoBehaviour
     [Header("Debug Flags")]
     [SerializeField]
     private bool movePlayer = false;
+    private bool iAmAirborn = false;
 
     void FixedUpdate()
     {
+        MoveJeep();
+        CorrectRotation();
+    }
+
+    void MoveJeep(){
         //Basic movement code for moving left and right.
         Vector3 strafeVector = transform.position + (Vector3.right * Time.deltaTime * strafeRate * Input.GetAxis("Horizontal"));
         Vector3 forwardVector = transform.position + (Vector3.forward * speed * Time.deltaTime);
@@ -36,14 +48,46 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void CorrectRotation(){
+        Quaternion currentRotation = transform.rotation;
+        #if UNITY_EDITOR
+            Debug.Log("Rotation of x is: " + currentRotation.eulerAngles.x);
+        #endif
+        if(iAmAirborn){
+            if(currentRotation.eulerAngles.x > rotationLimit){
+                Quaternion correctedRotation = Quaternion.Euler(
+                    currentRotation.x - (Time.deltaTime * rotationRate),
+                    currentRotation.y,
+                    currentRotation.z
+                );
+                rb.MoveRotation(correctedRotation);
+            }
+            else if(currentRotation.x < -rotationLimit){
+                Quaternion correctedRotation = Quaternion.Euler(
+                    currentRotation.x + (Time.deltaTime * rotationRate),
+                    currentRotation.y,
+                    currentRotation.z
+                );
+                rb.MoveRotation(correctedRotation);
+            }
+        }
+    }
+
     void OnCollisionEnter(Collision collision){
         bool tagIsObstacle = collision.gameObject.tag == "Obstacle";
         bool tagIsKillfield = collision.gameObject.tag == "killfield";
+        iAmAirborn = false;
         if(tagIsObstacle || tagIsKillfield){
             movePlayer = false;
             gameOverCanvas.gameObject.SetActive(true);
             Destroy(this.gameObject);
         }
 
+    }
+
+    void OnCollisionExit(Collision collision){
+        if(collision.contactCount == 0){
+            iAmAirborn = true;
+        }
     }
 }
